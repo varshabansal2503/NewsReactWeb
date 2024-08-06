7bfe4cd959628753799ac226c8567fff755165b7
varshabansal2503 <varshabansal25032503@gmail.com>
2024-08-06 10:28:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
